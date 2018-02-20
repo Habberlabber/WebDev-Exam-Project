c434f818c82f68155fd3c9c32db3b36172ded46a
Kasper Laursen <Habberlabber@gmail.com>
2018-02-20 12:05:52 +0100

    Initial routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { VotePageComponent } from './pages/vote-page/vote-page.component';
+
+const routes: Routes = [
+  { 
+    path: 'signup',
+    component: SignupPageComponent
+  },
+  { 
+    path: '',
+    component: VotePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { VotePageComponent } from './pages/vote-page/vote-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupPageComponent,
+    VotePageComponent
   ],
   imports: [
     BrowserModule,
