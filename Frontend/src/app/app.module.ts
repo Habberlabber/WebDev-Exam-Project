0f064edcc5f4ca718072a9d215e25e475bf1e1d2
Kasper Laursen <Habberlabber@gmail.com>
2018-02-20 14:33:30 +0100

    Basic form setup and validation implementet

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
@@ -16,7 +17,8 @@ import { VotePageComponent } from './pages/vote-page/vote-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/app/pages/signup-page/signup-page.component.ts
+++ Frontend/src/app/pages/signup-page/signup-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'WD-signup-page',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPageComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup; // Defien the formgroup
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
   }
 
+  createForm() {
+    this.signupForm = this.fb.group({
+      mail: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(128)])]
+      passwordRepeat: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(128)])],
+      birthday: ['', Validators.required],
+      firstname: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(32)])],
+      lastname: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(32)])],
+      description: ['', Validators.compose([Validators.required, Validators.minLength(60), Validators.maxLength(255)])]
+    });
+  }
 }
