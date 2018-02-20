01322456c0b6478d4eb6c73fd6e6e33bcdf9fa6e
Kasper Laursen <Habberlabber@gmail.com>
2018-02-20 17:35:21 +0100

    Component generated

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { VotePageComponent } from './pages/vote-page/vote-page.component';
+import { SigninPageComponent } from './pages/signin-page/signin-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupPageComponent,
-    VotePageComponent
+    VotePageComponent,
+    SigninPageComponent
   ],
   imports: [
     BrowserModule,
