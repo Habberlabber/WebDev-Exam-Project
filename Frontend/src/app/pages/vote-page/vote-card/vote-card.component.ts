805f454211191850479a6eb0f224c079cffd3853
Kasper Laursen <Habberlabber@gmail.com>
2018-02-22 11:07:30 +0100

    Image titles added

--- Frontend/src/app/pages/vote-page/vote-card/vote-card.component.ts
+++ Frontend/src/app/pages/vote-page/vote-card/vote-card.component.ts
@@ -15,28 +15,24 @@ export class VoteCardComponent implements OnInit {
   person:any = {
     firstname: "Test",
     lastname: " user",
-    desc: "Dette er en lang beskrivelse af en perosn Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem labore odio omnis adipisci nostrum optio dolorem odit qui vitae eius magni, voluptatum at debitis rerum mollitia eos dolor sequi repellendus!"
+    desc: "Dette er en lang beskrivelse af en perosn Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem labore odio omnis adipisci nostrum optio dolorem odit qui vitae eius magni, voluptatum at debitis rerum mollitia eos dolor sequi repellendus! Quidem labore odio omnis adipisci nostrum optio dolorem odit qui vitae eius magni, voluptatum at debitis rerum mollitia eos dolor sequi repellendus! Quidem labore odio omnis adipisci nostrum optio dolorem odit qui vitae eius magni, voluptatum at debitis rerum mollitia eos dolor sequi repellendus!"
     age: 26,
     images: [
     {
       url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
-      title: "This is me",
-      desc: "Ain't i a beauty?"
+      title: ""
     },
     {
       url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
-      title: "This is me",
-      desc: "Ain't i a beauty?"
+      title: "This is me 1"
     },
     {
       url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
-      title: "This is me",
-      desc: "Ain't i a beauty?"
+      title: ""
     },
     {
       url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
-      title: "This is me",
-      desc: "Ain't i a beauty?"
+      title: "This is me 2"
     }
     ]
   }
