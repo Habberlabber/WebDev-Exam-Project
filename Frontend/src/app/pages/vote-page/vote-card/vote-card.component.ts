98edf8ed9925f934ff7fe9f0747d1ea80f78f526
Kasper Laursen <Habberlabber@gmail.com>
2018-02-22 11:43:20 +0100

    Material icons added

--- Frontend/src/app/pages/vote-page/vote-card/vote-card.component.ts
+++ Frontend/src/app/pages/vote-page/vote-card/vote-card.component.ts
@@ -15,23 +15,23 @@ export class VoteCardComponent implements OnInit {
   person:any = {
     firstname: "Test",
     lastname: " user",
-    desc: "Hej med dig! Dette er en lang beskrivelse af en perosn Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem labore odio omnis adipisci nostrum optio dolorem odit qui vitae eius magni, voluptatum at debitis rerum mollitia eos dolor sequi repel!"
+    desc: "Hej med dig! Dette er en lang beskrivelse af en perosn Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem labore odio omnis adipisci nostrum optio dolorem odit qui vitae eius magni, voluptatum at debitis rerum mollitia eos dolor sequi repel!",
     age: 26,
     images: [
     {
-      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
+      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png",
       title: ""
     },
     {
-      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
+      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png",
       title: "This is me 1"
     },
     {
-      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
+      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png",
       title: ""
     },
     {
-      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
+      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png",
       title: "This is me 2"
     }
     ]
