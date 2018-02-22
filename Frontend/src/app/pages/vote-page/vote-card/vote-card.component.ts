944ea8d853076df456a254f9cb7334c86bcde210
Kasper Laursen <Habberlabber@gmail.com>
2018-02-22 10:57:36 +0100

    Initial card markup

--- Frontend/src/app/pages/vote-page/vote-card/vote-card.component.ts
+++ Frontend/src/app/pages/vote-page/vote-card/vote-card.component.ts
@@ -12,4 +12,33 @@ export class VoteCardComponent implements OnInit {
   ngOnInit() {
   }
 
+  person:any = {
+    firstname: "Test",
+    lastname: " user",
+    desc: "Dette er en lang beskrivelse af en perosn Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem labore odio omnis adipisci nostrum optio dolorem odit qui vitae eius magni, voluptatum at debitis rerum mollitia eos dolor sequi repellendus!"
+    age: 26,
+    images: [
+    {
+      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
+      title: "This is me",
+      desc: "Ain't i a beauty?"
+    },
+    {
+      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
+      title: "This is me",
+      desc: "Ain't i a beauty?"
+    },
+    {
+      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
+      title: "This is me",
+      desc: "Ain't i a beauty?"
+    },
+    {
+      url: "https://www.shareicon.net/download/512x512/2016/05/24/769972_people_512x512.png"
+      title: "This is me",
+      desc: "Ain't i a beauty?"
+    }
+    ]
+  }
+
 }
