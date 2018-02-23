4ddc3ba039e7530e63a329f845e120fd2fa7cf8d
Kasper Laursen <habberlabber@gmail.com>
2018-02-23 11:33:07 +0100

    Map component generated

--- Frontend/src/app/admin/admin.module.ts
+++ Frontend/src/app/admin/admin.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { UsersPageComponent } from './users-page/users-page.component';
+import { MapPageComponent } from './map-page/map-page.component';
 
 @NgModule({
   imports: [
@@ -13,6 +14,7 @@ import { UsersPageComponent } from './users-page/users-page.component';
   declarations: [
     DashboardPageComponent,
     UsersPageComponent,
+    MapPageComponent,
   ]
 })
 export class AdminModule { }
---
+++ Frontend/src/app/admin/map-page/map-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'WD-map-page',
+  templateUrl: './map-page.component.html',
+  styleUrls: ['./map-page.component.scss']
+})
+export class MapPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
